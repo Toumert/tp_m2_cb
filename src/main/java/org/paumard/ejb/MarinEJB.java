e62917734c837e5d4949c172b4f3be5a2f849a70
Toumert <toumert.mouloud@yahoo.fr>
2014-03-22 10:00:13 +0100

    Update MarinEJB.java
    Recherche d'un marin par son Id, et suppression

--- src/main/java/org/paumard/ejb/MarinEJB.java
+++ src/main/java/org/paumard/ejb/MarinEJB.java
@@ -1,8 +1,10 @@
 package org.paumard.ejb;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.paumard.model.Marin;
 
@@ -10,6 +12,8 @@ import org.paumard.model.Marin;
 public class MarinEJB {
 
 	@PersistenceContext(unitName="cloudbees-project")
+	@PersistenceContext(unitName="MySQL")
+	
 	private EntityManager em ;
 	
 	public Long createMarin(Marin marin) {
@@ -38,5 +42,28 @@ public class MarinEJB {
 			
 
 	}
++	public void deleteMarin(Marin marin ){
+
++		
+
++		Query q = em.createQuery("select Marin from Marin marin where marin.nom =" +"'" +marin.getNom()+"'" );
+
++		List<Marin> marins = q.getResultList() ;
+
++		for (Marin marin_sup : marins) {
+
++		   em.remove(marin_sup);
+
++		}}
+
++	public void rechercheMarin(Long Id ){
+
++		Marin marin=new Marin();
+
++		marin= em.find(Marin.class, Id);
+
++					
+
++	}
 
 }
